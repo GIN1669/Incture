51c54e0e74120f7bc9cca68317402cfa5431bcfc
Sathwik gowda BR <sathwikgowdabr16404@gmail.com>
2025-06-30 11:10:16 +0530

    v5

--- src/com/inc/two.java
+++ src/com/inc/two.java
@@ -2,9 +2,17 @@ package com.inc;
 
 public class two {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class Two {
+	    public static void main(String[] args) {
+	        int num1 = 10;
+	        int num2 = 20;
+	        int num3 = 30;
 
+	        int sum = num1 + num2 + num3;
+
+	        System.out.println("The sum of the three numbers is: " + sum);
+	    }
 	}
+
 
 }
